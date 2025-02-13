0989bb687342f4346606455a87137fd046520c9c
Jem <jemstobart@hotmail.com>
2025-02-13 11:01:31 +0000

    Add test and fix logging

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ SECRET_ACCESS_KEY = os.getenv('SECRET_ACCESS_KEY')
 TEAMS_WEBHOOK = os.getenv('TEAMS_WEBHOOK')
 
 logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 def process_message():
     sqs = boto3.client('sqs', region_name=AWS_REGION, aws_access_key_id=ACCESS_KEY ,
@@ -30,34 +31,37 @@ def process_message():
         MaxNumberOfMessages=1,
         WaitTimeSeconds=20
     )
-    logger.info("Message received from queue with ID" + response.get('MessageId'))
 
     messages = response.get('Messages')
     if messages is not None:
         message = messages[0]
+        logger.info("Message received from queue with ID" + json.dumps(response.get('MessageId')))
         body = json.loads(message['Body'])
-        if body["title"] and body["description"]:
-            payload = {
-                "@type": "MessageCard",
-                "@context": "http://schema.org/extensions",
-                "themeColor": "FF0000",
-                "title": body["title"],
-                "text": body["description"]
-            }
+        if "title" in body and "description" in body:
+            if body["title"] and body["description"]:
+                payload = {
+                    "@type": "MessageCard",
+                    "@context": "http://schema.org/extensions",
+                    "themeColor": "FF0000",
+                    "title": body["title"],
+                    "text": body["description"]
+                }
 
-            teams_message = pymsteams.connectorcard(TEAMS_WEBHOOK)
-            teams_message.payload = payload
-            logger.info("Sending Teams alert")
-            teams_message.send()
-            logger.info("Teams alert sent, payload" + json.dumps(payload))
+                teams_message = pymsteams.connectorcard(TEAMS_WEBHOOK)
+                teams_message.payload = payload
+                logger.info("Sending Teams alert")
+                teams_message.send()
+                logger.info("Teams alert sent, payload" + json.dumps(payload))
+            else:
+                logger.error("Either the title or description or both are empty")
         else:
-            logger.info("Either the title or description or both are missing from the SQS message")
+            logger.error("Either the title or description or both are missing from the SQS message")
 
         sqs.delete_message(
             QueueUrl=P1_QUEUE,
             ReceiptHandle=message['ReceiptHandle']
         )
-        logger.info("Message deleted from queue with ID:" + response.get('MessageId'))
+        logger.info("Message deleted from queue with ID:" + json.dumps(response.get('MessageId')))
     else:
         logger.info("No messages in queue")
 
--- tests/test.py
+++ tests/test.py
@@ -29,3 +29,25 @@ def test_process_message(connectorcard_mock, sqs_client):
     messages = sqs_messages.get('Messages')
     assert connectorcard_mock.called
     assert messages is None
+
+@patch('pymsteams.connectorcard')
+def test_process_message_wrong_data(connectorcard_mock, sqs_client):
+    queue = sqs_client.create_queue(QueueName='queue')
+
+    queue_url = queue['QueueUrl']
+
+    # override function global URL variable
+    main.P1_QUEUE = queue_url
+
+    connectorcard_mock.return_value = FakeConnectorCard()
+
+    expected_msg = json.dumps({'description': 'Happening right now'})
+
+    sqs_messages = sqs_client.send_message(QueueUrl=queue_url, MessageBody=expected_msg)
+
+    main.process_message()
+
+    messages = sqs_messages.get('Messages')
+
+    assert connectorcard_mock.called == False
+    assert messages is None
