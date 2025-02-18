2a8037f6a37e60a110f4bf32ddb4d4dd820679d5
Jem <jemstobart@hotmail.com>
2025-02-18 12:54:04 +0000

    Try to fix threading

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,13 @@
-FROM python:latest
+FROM python:3.12-slim
 WORKDIR /Priority1
 COPY . /Priority1
 RUN pip3 install -r requirements.txt
 ENV FLASK_APP=main
+ENV PYTHONUNBUFFERED=1
 ENV AWS_REGION=''
 ENV P1_QUEUE=''
 ENV ACCESS_KEY=''
 ENV SECRET_ACCESS_KEY=''
 ENV TEAMS_WEBHOOK=''
 EXPOSE 8000
-CMD ["flask", "run", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["gunicorn", "--bind","0.0.0.0:8000", "main:app"]
--- main.py
+++ main.py
@@ -21,54 +21,64 @@ TEAMS_WEBHOOK = os.getenv('TEAMS_WEBHOOK')
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
+stop_flag = False
+
 def process_message():
-    sqs = boto3.client('sqs', region_name=AWS_REGION, aws_access_key_id=ACCESS_KEY ,
-                       aws_secret_access_key=SECRET_ACCESS_KEY)
+    global stop_flag
+    while not stop_flag:
+        sqs = boto3.client('sqs', region_name=AWS_REGION, aws_access_key_id=ACCESS_KEY ,
+                           aws_secret_access_key=SECRET_ACCESS_KEY)
 
-    response = sqs.receive_message(
-        QueueUrl=P1_QUEUE,
-        MessageAttributeNames=['All'],
-        MaxNumberOfMessages=1,
-        WaitTimeSeconds=20
-    )
-
-    messages = response.get('Messages')
-    if messages is not None:
-        message = messages[0]
-        logger.info("Message received from queue with ID" + json.dumps(response.get('MessageId')))
-        body = json.loads(message['Body'])
-        if "title" in body and "description" in body:
-            if body["title"] and body["description"]:
-                payload = {
-                    "@type": "MessageCard",
-                    "@context": "http://schema.org/extensions",
-                    "themeColor": "FF0000",
-                    "title": body["title"],
-                    "text": body["description"]
-                }
-
-                teams_message = pymsteams.connectorcard(TEAMS_WEBHOOK)
-                teams_message.payload = payload
-                logger.info("Sending Teams alert")
-                teams_message.send()
-                logger.info("Teams alert sent, payload" + json.dumps(payload))
-            else:
-                logger.error("Either the title or description or both are empty")
-        else:
-            logger.error("Either the title or description or both are missing from the SQS message")
-
-        sqs.delete_message(
+        response = sqs.receive_message(
             QueueUrl=P1_QUEUE,
-            ReceiptHandle=message['ReceiptHandle']
+            MessageAttributeNames=['All'],
+            MaxNumberOfMessages=1,
+            WaitTimeSeconds=20
         )
-        logger.info("Message deleted from queue with ID:" + json.dumps(response.get('MessageId')))
-    else:
-        logger.info("No messages in queue")
+
+        messages = response.get('Messages')
+        if messages is not None:
+            message = messages[0]
+            logger.info("Message received from queue with ID" + json.dumps(response.get('MessageId')))
+            body = json.loads(message['Body'])
+            if "title" in body and "description" in body:
+                if body["title"] and body["description"]:
+                    payload = {
+                        "@type": "MessageCard",
+                        "@context": "http://schema.org/extensions",
+                        "themeColor": "FF0000",
+                        "title": body["title"],
+                        "text": body["description"]
+                    }
+
+                    teams_message = pymsteams.connectorcard(TEAMS_WEBHOOK)
+                    teams_message.payload = payload
+                    logger.info("Sending Teams alert")
+                    teams_message.send()
+                    logger.info("Teams alert sent, payload" + json.dumps(payload))
+                else:
+                    logger.error("Either the title or description or both are empty")
+            else:
+                logger.error("Either the title or description or both are missing from the SQS message")
+
+            sqs.delete_message(
+                QueueUrl=P1_QUEUE,
+                ReceiptHandle=message['ReceiptHandle']
+            )
+            logger.info("Message deleted from queue with ID:" + json.dumps(response.get('MessageId')))
+        else:
+            logger.info("No messages in queue")
 
 if __name__ == '__main__':
-    threading.Thread(target=process_message, daemon=True).start()
-    app.run()
+    thread = threading.Thread(target=process_message, daemon=True)
+    thread.start()
+    try:
+        app.run(host="0.0.0.0")
+    except KeyboardInterrupt:
+        logger.info("Shutting down...")
+        stop_flag = True
+        thread.join()
 
-@app.route('/health', methods=['GET'])
+@app.route('/', methods=['GET'])
 def health_check():
     return 'OK', 200
